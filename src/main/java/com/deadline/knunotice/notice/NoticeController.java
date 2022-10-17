54f9d7223d1adb8490c52e9b99b03f6c6903905f
YoonSuhyeok <cooltime15@likelion.org>
2022-10-17 12:08:01 +0900

    Update NoticeController - record & search Method

--- src/main/java/com/deadline/knunotice/notice/NoticeController.java
+++ src/main/java/com/deadline/knunotice/notice/NoticeController.java
@@ -1,14 +1,16 @@
 package com.deadline.knunotice.notice;
 
+import com.deadline.knunotice.major.MajorResponseDTO;
+import com.deadline.knunotice.member.MemberAuthentication;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/notice")
@@ -21,19 +23,48 @@ public class NoticeController {
     }
 
     @GetMapping()
-    public ResponseEntity<Page<NoticeResponseDTO>> noticeFindAll(@PageableDefault(size=20) Pageable pageable){
-        Page<NoticeResponseDTO> notices = noticeService.findAll(pageable);
+    public ResponseEntity<Page<NoticeAndETC>> noticeFindAll(@PageableDefault(size=20) Pageable pageable){
+        Page<NoticeAndETC> notices = noticeService.findAll(pageable);
         return new ResponseEntity<>(notices, HttpStatus.OK);
     }
 
-    @GetMapping("/search")
-    public ResponseEntity<Page<NoticeResponseDTO>> searchAll(@Param("keyword") String keyword, @PageableDefault(size=20) Pageable pageable){
-        Page<NoticeResponseDTO> notices;
-        if(keyword == null) {
-            notices = noticeService.findAll(pageable);
-        } else {
-            notices = noticeService.searchAll(keyword, pageable);
+    @PostMapping("/search")
+    public ResponseEntity<?> searchAll(MemberAuthentication memberAuthentication, @Param("keyword") String keyword, @PageableDefault(size=20) Pageable pageable, @RequestBody ArrayList<MajorResponseDTO> majors)
+    {
+        Page<NoticeAndETC> notices;
+
+        // 멤버인 경우
+        if(memberAuthentication != null) {
+            // 전체 조회
+            if(keyword == null && majors.get(0).getId() == 0) {
+                notices = noticeService.findAllVersionSQL(memberAuthentication.getMember().getId(), keyword, pageable);
+            } else {
+                notices = noticeService.findAllVersionSQLAndMajor(memberAuthentication.getMember().getId(), keyword, majors, pageable);
+            }
         }
+        // 멤버가 아닌 경우
+        else {
+            if(keyword == null && majors.get(0).getId() == 0 ){
+                notices = noticeService.findAll(pageable);
+            } else if(keyword != null && majors.get(0).getId() == 0) {
+                notices = noticeService.searchAll(keyword, pageable);
+            } else if(keyword == null && majors.get(0).getId() != 0) {
+                notices = noticeService.findAllForMajors(majors, pageable);
+            } else {
+                //notices = noticeService.findAllVersionSQLAndMajor()
+                notices = null;
+            }
+        }
+
         return new ResponseEntity<>(notices, HttpStatus.OK);
+    }
+
+    @PostMapping("/record")
+    public ResponseEntity<Integer> recodePin(MemberAuthentication memberAuthentication,
+                                       @Param("noticeId") Long noticeId, @Param("type") Integer type) {
+
+        Integer result = noticeService.recodePinOrBookmark(memberAuthentication.getMember(), noticeId, type);
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
