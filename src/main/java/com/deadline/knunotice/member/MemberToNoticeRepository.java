491c6bc533e945a2ba98a2bcf2fb5c66f0c5a5b7
YoonSuhyeok <cooltime15@likelion.org>
2022-10-17 12:20:30 +0900

    Update MemberToNoticeRepository

--- src/main/java/com/deadline/knunotice/member/MemberToNoticeRepository.java
+++ src/main/java/com/deadline/knunotice/member/MemberToNoticeRepository.java
@@ -1,8 +1,14 @@
 package com.deadline.knunotice.member;
 
+import com.deadline.knunotice.notice.Notice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberToNoticeRepository extends JpaRepository<MemberToNotice, Long> {
+
+    Optional<MemberToNotice> findMemberToNoticeByMemberAndNotice(Member member, Notice notice);
+
 }
