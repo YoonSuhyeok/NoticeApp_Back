ae30f8206690a7da10469dc1b77dfff2a9d579f7
YoonSuhyeok <cooltime15@likelion.org>
2022-10-17 12:16:15 +0900

    Update NoticeService

--- src/main/java/com/deadline/knunotice/notice/NoticeService.java
+++ src/main/java/com/deadline/knunotice/notice/NoticeService.java
@@ -1,12 +1,24 @@
 package com.deadline.knunotice.notice;
 
+import com.deadline.knunotice.major.MajorResponseDTO;
+import com.deadline.knunotice.member.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.ArrayList;
+
 public interface NoticeService {
 
-     Page<NoticeResponseDTO> findAll(Pageable pageable);
+     Page<NoticeAndETC> findAll(Pageable pageable);
 
-     Page<NoticeResponseDTO> searchAll(String keyword, Pageable pageable);
+     Page<NoticeAndETC> searchAll(String keyword, Pageable pageable);
 
+    Integer recodePinOrBookmark(Member member, Long noticeId, Integer type);
+
+    Page<NoticeAndETC> findAllVersionSQL(Long id, String keyword, Pageable pageable);
+
+    Page<NoticeAndETC> findAllVersionSQLAndMajor(Long id, String keyword, ArrayList<MajorResponseDTO> majors, Pageable pageable);
+
+    Page<NoticeAndETC> findAllForMajors(ArrayList<MajorResponseDTO> majors, Pageable pageable);
+    
 }
--- src/main/java/com/deadline/knunotice/notice/NoticeServiceImpl.java
+++ src/main/java/com/deadline/knunotice/notice/NoticeServiceImpl.java
@@ -1,29 +1,116 @@
 package com.deadline.knunotice.notice;
 
+import com.deadline.knunotice.major.MajorResponseDTO;
+import com.deadline.knunotice.member.Member;
+import com.deadline.knunotice.member.MemberToNotice;
+import com.deadline.knunotice.member.MemberToNoticeRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class NoticeServiceImpl implements NoticeService {
 
     private final NoticeRepository noticeRepository;
 
-    public NoticeServiceImpl(NoticeRepository noticeRepository) {
+    private final MemberToNoticeRepository mtnRepository;
+
+    public NoticeServiceImpl(NoticeRepository noticeRepository, MemberToNoticeRepository mtnRepository) {
         this.noticeRepository = noticeRepository;
+        this.mtnRepository = mtnRepository;
     }
 
     @Override
-    public Page<NoticeResponseDTO> findAll(Pageable pageable) {
+    public Page<NoticeAndETC> findAll(Pageable pageable) {
         // TODO 생성일 기준 정렬 필요
-        Page<Notice> notices = noticeRepository.findAll(pageable);
-        return NoticeResponseDTO.builder().build().toDtoPage(notices);
+        return noticeRepository.findNoticeDto(null, pageable);
     }
 
     @Override
-    public Page<NoticeResponseDTO> searchAll(String keyword, Pageable pageable) {
-        Page<Notice> notices = noticeRepository.findAllByTitleContainingOrderByCreatedDate(keyword, pageable);
-        return NoticeResponseDTO.builder().build().toDtoPage(notices);
+    public Page<NoticeAndETC> searchAll(String keyword, Pageable pageable) {
+        return noticeRepository.findNoticeDtoForKeyword(null, keyword, pageable);
+    }
+
+    @Override
+    public Page<NoticeAndETC> findAllVersionSQL(Long id, String keyword, Pageable pageable) {
+
+        Page<NoticeAndETC> notices;
+        if(keyword == null) {
+            notices = noticeRepository.findNoticeDto(id, pageable);
+        } else {
+            notices = noticeRepository.findNoticeDtoForKeyword(id, keyword, pageable);
+        }
+
+        return notices;
+    }
+
+    @Override
+    public Page<NoticeAndETC> findAllVersionSQLAndMajor(Long id, String keyword, ArrayList<MajorResponseDTO> majors, Pageable pageable) {
+        Page<NoticeAndETC> notices;
+        if(keyword == null) {
+            notices = noticeRepository.findNoticeDto(id, pageable);
+        } else {
+            notices = noticeRepository.findNoticeDtoForKeyword(id, keyword, pageable);
+        }
+        return notices;
+    }
+
+    @Override
+    public Page<NoticeAndETC> findAllForMajors(ArrayList<MajorResponseDTO> majors, Pageable pageable) {
+        // 문자열 정리
+        List<String> m = new ArrayList<>();
+        StringBuilder query = new StringBuilder();
+        for(MajorResponseDTO major: majors) {
+            query.append("'").append(major.getId()).append("'").append(",");
+            m.add(major.getId().toString());
+        }
+        System.out.println(m);
+        query = new StringBuilder(query.substring(0, query.length() - 1));
+        System.out.println(query);
+
+        return noticeRepository.findAllByMajor(m, pageable);
+    }
+
+    @Override
+    public Integer recodePinOrBookmark(Member member, Long noticeId, Integer type) {
+
+        Notice notice = noticeRepository.findById(noticeId).orElseThrow();
+
+        MemberToNotice mtn = mtnRepository.findMemberToNoticeByMemberAndNotice(member, notice).orElse(null);
+
+        if(mtn == null) {
+            mtn = MemberToNotice.builder()
+                    .member(member)
+                    .notice(notice)
+                    .isPin(false)
+                    .isBookmark(false)
+                    .build();
+        }
+
+        if(type == 0) {
+            mtn.setPin(!mtn.isPin());
+        } else {
+            mtn.setBookmark(!mtn.isBookmark());
+        }
+
+        mtnRepository.save(mtn);
+        if(type == 0) {
+            if(mtn.isPin()){
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            if(mtn.isBookmark()){
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
     }
 
 }
